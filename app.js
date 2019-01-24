0728c79276b5b58dee2185f3869ab602fbb623fa
赵志强 <13253126119@qq.com>
2019-01-24 16:15:41 +0800

    mode change app.js v1.6

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const fly = require('fly');
 const path = require('path');
 const http = require('http');
+const fs = require('fs');
+
+console.log(fs)
 
 http.createServer(()=>{
     console.log('server')
 },8080)
+
+
